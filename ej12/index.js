5e1de92db1e621adbcd0573c0ad2fc505231e24b
Dagore <david.gomez.redondo.dev@gmail.com>
2023-01-28 21:40:33 +0100

    ej2

--- ej12/index.js
+++ ej12/index.js
@@ -1,14 +1,3 @@
-// Turn this functions into arrow functions
-// function ask(question, yes, no) {
-//   if (confirm(question)) yes()
-//   else no()
-// }
+const getPi = () => Number(Math.PI.toFixed(2))
 
-// ask(
-//   "Do you agree?",
-//   function () { alert("You agreed.") },
-//   function () { alert("You canceled the execution.") }
-// )
-
-const ask = (question, yes, no) => confirm(question) ? yes() : no()
-ask('Do you agree?', () => alert('You agreed.'), () => alert('You canceled the execution.'))
+console.log(getPi())
