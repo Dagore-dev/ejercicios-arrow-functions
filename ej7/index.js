4cecca9bcd8db23b843606f98baf5e17153e739c
Dagore <david.gomez.redondo.dev@gmail.com>
2023-01-28 12:59:09 +0100

    ej7

--- ej7/index.js
+++ ej7/index.js
@@ -1,14 +1,20 @@
-// Turn this functions into arrow functions
-// function ask(question, yes, no) {
-//   if (confirm(question)) yes()
-//   else no()
-// }
+const getMeanFromIntArray = (intArray) => {
+  if (intArray.length === 0) {
+    throw new Error('Array is empty')
+  }
 
-// ask(
-//   "Do you agree?",
-//   function () { alert("You agreed.") },
-//   function () { alert("You canceled the execution.") }
-// )
+  return intArray.reduce((accumulator, current) => accumulator + current) / intArray.length
+}
 
-const ask = (question, yes, no) => confirm(question) ? yes() : no()
-ask('Do you agree?', () => alert('You agreed.'), () => alert('You canceled the execution.'))
+const a1 = [1, 2, 3, 4, 5]
+const a1Mean = getMeanFromIntArray(a1)
+
+const a2 = [1, 1, 1]
+const a2Mean = getMeanFromIntArray(a2)
+
+const a3 = [-7]
+const a3Mean = getMeanFromIntArray(a3)
+
+console.log(a1, 'Mean =', a1Mean)
+console.log(a2, 'Mean =', a2Mean)
+console.log(a3, 'Mean =', a3Mean)
