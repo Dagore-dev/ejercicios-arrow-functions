2ca5645da756e1578bcb75500330d9d1387f6778
Dagore <david.gomez.redondo.dev@gmail.com>
2023-01-28 22:03:24 +0100

    ej16

--- ej16/index.js
+++ ej16/index.js
@@ -1,14 +1,14 @@
-// Turn this functions into arrow functions
-// function ask(question, yes, no) {
-//   if (confirm(question)) yes()
-//   else no()
-// }
+const randomIntBetween = (min = 1, max = 100) => max > min ? Math.floor((Math.random() * max) + min) : NaN
 
-// ask(
-//   "Do you agree?",
-//   function () { alert("You agreed.") },
-//   function () { alert("You canceled the execution.") }
-// )
+console.log('Testing... Please wait')
 
-const ask = (question, yes, no) => confirm(question) ? yes() : no()
-ask('Do you agree?', () => alert('You agreed.'), () => alert('You canceled the execution.'))
+for (let i = -1_000_000; i < 1_000_001; i++) {
+  const random = randomIntBetween(1, i)
+  console.log(random)
+
+  if (random < 1 || random > i) {
+    throw new Error(`In run ${i} "${random}" is out of range`)
+  }
+}
+
+console.log('All ok')
