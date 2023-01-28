e71ec4c71d2849db6bd14240771a0ec0906d2d10
Dagore <david.gomez.redondo.dev@gmail.com>
2023-01-28 18:59:28 +0100

    ej10

--- ej10/index.js
+++ ej10/index.js
@@ -1,14 +1,4 @@
-// Turn this functions into arrow functions
-// function ask(question, yes, no) {
-//   if (confirm(question)) yes()
-//   else no()
-// }
+const getTime = (distance, speed) => distance / speed
 
-// ask(
-//   "Do you agree?",
-//   function () { alert("You agreed.") },
-//   function () { alert("You canceled the execution.") }
-// )
-
-const ask = (question, yes, no) => confirm(question) ? yes() : no()
-ask('Do you agree?', () => alert('You agreed.'), () => alert('You canceled the execution.'))
+const getTimeForSeventyThreeThousandKilometersPerHourAndOneHundredAndTwentyKilometers = () => getTime(73000, 120)
+console.log(getTimeForSeventyThreeThousandKilometersPerHourAndOneHundredAndTwentyKilometers())
